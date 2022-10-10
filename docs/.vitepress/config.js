cc38221e4015185b616d475ec2d5b9672b428bc9
yuka <yukayu@yeahka.com>
2022-10-10 22:48:56 +0800

    docs: update

--- docs/.vitepress/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    title: '笔记',
-    base: '/yuka-sen/'
-}
---
+++ src/.vitepress/config.js
@@ -0,0 +1,7 @@
+const { resolve } = require('path')
+
+module.exports = {
+    title: '笔记',
+    base: '/yuka-sen/',
+    outDir: resolve(__dirname, '../../docs')
+}
